ae3c163e308caf041cb98b566face2fc59d44180
Angel Andrade <angel.bruno.andrade.osorio@gmail.com>
2026-06-19 21:00:52 -0400

    Add initial project structure with configuration and example files for DHT11 sensor

---
+++ hw-477-v0.2-DHT11/src/main.cpp
@@ -0,0 +1,36 @@
+#include <DHT.h>
+
+#define DHT_PIN 2
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  Serial.println("DHT11 test started");
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperatureC = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperatureC)) {
+    Serial.println("Failed to read from DHT sensor");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" °C");
+
+  Serial.println("---");
+
+  delay(2000);
+}
